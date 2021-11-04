8079557c13b4aefde30252f824575600cefe5cc0
Ilya-Legchilin <ilya2012legchilin@yandex.ru>
2021-11-04 17:03:45 +0300

    second instruction initial commit

--- src/processor.rs
+++ src/processor.rs
@@ -4,8 +4,10 @@ use std::convert::TryFrom;
 
 use solana_program::{
     account_info::{next_account_info, AccountInfo},
+    program::invoke_signed,
     entrypoint::ProgramResult,
     pubkey::Pubkey,
+    system_instruction,
 };
 use solana_program::msg;
 
@@ -14,8 +16,11 @@ use borsh::{BorshSerialize, BorshDeserialize};
 
 #[derive(BorshSerialize, BorshDeserialize)]
 pub struct Data {
+    pub program_id: Pubkey,
     pub flag: bool,
     pub amount: u64,
+    pub x: u64,
+    pub y: u64,
 }
 
 /// Instruction processor
@@ -25,21 +30,54 @@ pub fn process_instruction(
     instruction_data: &[u8],
 ) -> ProgramResult {
     let account_info_iter = &mut accounts.iter();
+
+    let system_program_info = next_account_info(account_info_iter)?;
+    let payer_info = next_account_info(account_info_iter)?;
     let source_info = next_account_info(account_info_iter)?;
     let destination_info = next_account_info(account_info_iter)?;
-    let pda_x_info = next_account_info(account_info_iter)?;
-    let pda_y_info = next_account_info(account_info_iter)?;
-    let Data {flag, amount} = Data::try_from_slice(instruction_data)?;
-    println!("data: {:?}", instruction_data);
-    let x = **pda_x_info.try_borrow_mut_lamports()?;
-    let y = **pda_y_info.try_borrow_mut_lamports()?;
+    let allocated_x_info = next_account_info(account_info_iter)?;
+    let allocated_y_info = next_account_info(account_info_iter)?;
+    let Data {
+        program_id,
+        flag, 
+        amount, 
+        x, 
+        y, 
+    } = Data::try_from_slice(instruction_data)?;
+    let payer_key = payer_info.key;
+    
+    invoke_signed(
+        &system_instruction::transfer(payer_key, allocated_x_info.key, x),
+        &[
+            payer_info.clone(),
+            allocated_x_info.clone(),
+        ],
+        &[&[b"You pass butter x", &[255]]],
+    )?;
+    invoke_signed(
+        &system_instruction::transfer(payer_key, allocated_y_info.key, y),
+        &[
+            payer_info.clone(),
+            allocated_y_info.clone(),
+        ],
+        &[&[b"You pass butter y", &[254]]],
+    )?;
+    invoke_signed(
+        &system_instruction::assign(allocated_y_info.key, &program_id),
+        &[
+            payer_info.clone(),
+            allocated_y_info.clone(),
+        ],
+        &[&[b"You pass butter y", &[254]]],
+    )?;
+
     let k = x * y;
     let dy = y - k/(x + amount);
     msg!("dx: {}, dy: {}", amount, dy);
     **source_info.try_borrow_mut_lamports()? -= amount;
-    **pda_x_info.try_borrow_mut_lamports()? += amount;
+    **allocated_x_info.try_borrow_mut_lamports()? += amount;
+    **allocated_y_info.try_borrow_mut_lamports()? -= dy;
     **destination_info.try_borrow_mut_lamports()? += dy;
-    **pda_y_info.try_borrow_mut_lamports()? -= dy;
 
     Ok(())
 }
--- tests/functional.rs
+++ tests/functional.rs
@@ -2,6 +2,7 @@ use {
     solana_program::{
         instruction::{AccountMeta, Instruction},
         pubkey::Pubkey,
+        system_program,
     },
     solana_program_test::*,
     solana_sdk::{account::Account, signature::Signer, transaction::Transaction},
@@ -14,41 +15,27 @@ use test_aloha::processor::Data;
 #[tokio::test]
 async fn test_lamport_transfer() {
     let program_id = Pubkey::from_str("TransferLamports111111111111111111111111111").unwrap();
-    let pubkey_pda_x = Pubkey::new_unique();
-    let pubkey_pda_y = Pubkey::new_unique();
+    let (allocated_pubkey_x, bump_seed_x) =
+        Pubkey::find_program_address(&[b"You pass butter x"], &program_id);
+    let (allocated_pubkey_y, bump_seed_y) =
+        Pubkey::find_program_address(&[b"You pass butter y"], &program_id);
     let pubkey_user_a = Pubkey::new_unique();
     let pubkey_user_b = Pubkey::new_unique();
-    let lamp_pda_x = 10_000_000; // these amounts are not actual amount of tokens but 0.00001 part 
-    let lamp_pda_y = 100_000_000;
     let lamp_user_a = 1_000_000;
     let lamp_user_b = 2_000_000;
+    let lamp_x = 10_000_000;
+    let lamp_y = 20_000_000;
     let dx:u64 = 1_998;
 
     let mut program_test = ProgramTest::new(
         "test_aloha",
         program_id,
         processor!(process_instruction),
-    );
-    program_test.add_account(
-        pubkey_pda_x,
-        Account {
-            lamports: lamp_pda_x,
-            owner: program_id,
-            ..Account::default()
-        },
-    );
+    );    println!("bump seed x: {}", bump_seed_x);
     program_test.add_account(
         pubkey_user_a,
         Account {
             lamports: lamp_user_a,
-            owner: program_id,
-            ..Account::default()
-        },
-    );
-    program_test.add_account(
-        pubkey_pda_y,
-        Account {
-            lamports: lamp_pda_y,
             owner: program_id,
             ..Account::default()
         },
@@ -62,43 +49,43 @@ async fn test_lamport_transfer() {
         },
     );
     let (mut banks_client, payer, recent_blockhash) = program_test.start().await;
-
-    let account_a = banks_client.get_account(pubkey_user_a).await.unwrap().unwrap();
-    let account_b = banks_client.get_account(pubkey_user_b).await.unwrap().unwrap();
-    let account_x = banks_client.get_account(pubkey_pda_x).await.unwrap().unwrap();
-    let account_y = banks_client.get_account(pubkey_pda_y).await.unwrap().unwrap();
-    println!("LAMP A: {}", account_a.lamports);
-    println!("LAMP B: {}", account_b.lamports);
-    println!("LAMP X: {}", account_x.lamports);
-    println!("LAMP Y: {}", account_y.lamports);
-    println!("dx: {}", dx);
+    let pubkey_payer = payer.pubkey();
     
      let v = Data {
+        program_id: program_id,
         flag: true,
         amount: dx,
-    }; 
+        x: lamp_x,
+        y: lamp_y,
+    };
     let mut transaction = Transaction::new_with_payer(
         &[Instruction::new_with_borsh(
             program_id,
             &v,
             vec![
+                AccountMeta::new(system_program::id(), false),
+                AccountMeta::new(pubkey_payer, false),
                 AccountMeta::new(pubkey_user_a, false),
                 AccountMeta::new(pubkey_user_b, false),
-                AccountMeta::new(pubkey_pda_x, false),
-                AccountMeta::new(pubkey_pda_y, false),
+                AccountMeta::new(allocated_pubkey_x, false),
+                AccountMeta::new(allocated_pubkey_y, false),
             ],
         )],
         Some(&payer.pubkey()),
     );
     transaction.sign(&[&payer], recent_blockhash);
     banks_client.process_transaction(transaction).await.unwrap();
-
-    let account_a = banks_client.get_account(pubkey_user_a).await.unwrap().unwrap();
-    let account_b = banks_client.get_account(pubkey_user_b).await.unwrap().unwrap();
-    let account_x = banks_client.get_account(pubkey_pda_x).await.unwrap().unwrap();
-    let account_y = banks_client.get_account(pubkey_pda_y).await.unwrap().unwrap();
-    println!("LAMP A: {}", account_a.lamports);
-    println!("LAMP B: {}", account_b.lamports);
-    println!("LAMP X: {}", account_x.lamports);
-    println!("LAMP Y: {}", account_y.lamports);
+   
+    let account_user_a = banks_client.get_account(pubkey_user_a).await.unwrap().unwrap();
+    let account_user_b = banks_client.get_account(pubkey_user_b).await.unwrap().unwrap();
+    let account_allocated_x = banks_client.get_account(allocated_pubkey_x).await.unwrap().unwrap();
+    let account_allocated_y = banks_client.get_account(allocated_pubkey_y).await.unwrap().unwrap();
+    let lamp_user_a = account_user_a.lamports;
+    let lamp_user_b = account_user_b.lamports;
+    let lamp_all_x = account_allocated_x.lamports;
+    let lamp_all_y = account_allocated_y.lamports;
+    println!("lamp user a: {}", lamp_user_a);
+    println!("lamp user b: {}", lamp_user_b);
+    println!("lamp allocated x: {}", lamp_all_x);
+    println!("lamp allocated y: {}", lamp_all_y);
 }
